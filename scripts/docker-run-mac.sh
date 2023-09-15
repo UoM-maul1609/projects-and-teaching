82b223fc3077f8644381e69205594887b6da02d7
Paul Connolly <p.connolly@manchester.ac.uk>
2023-09-15 16:06:06 +0100

    updated xhost command

--- scripts/docker-run-mac.sh
+++ scripts/docker-run-mac.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 ssh-keygen -R [127.0.0.1]:2022
-xhost +
+xhost +local:docker
 docker run  -it --rm -e DISPLAY=host.docker.internal:0 -p 2022:22 -v /tmp/.x11-unix:/tmp/.x11-unix ghcr.io/uom-maul1609/projects-and-teaching-all
