4e3c59e6702d7e5178ed4791970543bb112fab2e
Paul Connolly <p.connolly@manchester.ac.uk>
2023-09-15 16:58:27 +0100

    updated

--- scripts/set-up-chromebook-docker.sh
+++ scripts/set-up-chromebook-docker.sh
@@ -1,6 +1,8 @@
-sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update -y
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get install -y lsb-release software-properties-common
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
