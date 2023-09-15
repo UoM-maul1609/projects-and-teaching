b72b4e9f06358a458e3372ca17a4b022389a82f5
Paul Connolly <p.connolly@manchester.ac.uk>
2023-09-15 15:58:33 +0100

    updated scripts for running and dockerfile for amd64/arm64v8

--- scripts/docker-run-mac.sh
+++ scripts/docker-run-mac.sh
@@ -2,4 +2,4 @@
 
 ssh-keygen -R [127.0.0.1]:2022
 xhost +
-docker run  -it --rm -e DISPLAY=host.docker.internal:0 -p 2022:22 -v /tmp/.x11-unix:/tmp/.x11-unix ghcr.io/uom-maul1609/projects-and-teaching
+docker run  -it --rm -e DISPLAY=host.docker.internal:0 -p 2022:22 -v /tmp/.x11-unix:/tmp/.x11-unix ghcr.io/uom-maul1609/projects-and-teaching-all
