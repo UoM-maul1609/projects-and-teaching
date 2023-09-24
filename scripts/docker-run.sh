1f9a1943eb1d9b633619ba3e852dfc83ce16ecb6
Paul Connolly <p.connolly@manchester.ac.uk>
2023-09-24 20:59:06 +0100

    added general file

---
+++ scripts/docker-run.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+ssh-keygen -R [127.0.0.1]:2022
+xhost +local:docker
+docker run  -it --rm -e DISPLAY=host.docker.internal:0 --net=host -p 2022:22 -v /tmp/.x11-unix:/tmp/.x11-unix ghcr.io/uom-maul1609/projects-and-teaching-all
